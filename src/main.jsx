07d00d05249ce114dd7418911481c1b475548e0b
ChrisNieves <nievescs20@gmail.com>
2023-09-08 11:52:48 -0400

    update to main.jsx & store. provider conflict fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
-import { productsApi } from "./store/apiSlice.js";
+// import { ApiProvider } from "@reduxjs/toolkit/dist/query/react";
+// import { productsApi } from "./store/apiSlice.js";
 import { BrowserRouter } from "react-router-dom";
 import store from "./store/index.js";
 import App from "./App.jsx";
@@ -11,11 +11,11 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <ApiProvider api={productsApi}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </ApiProvider>
+      {/* <ApiProvider api={productsApi}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </ApiProvider> */}
     </Provider>
   </React.StrictMode>
 );
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./counterSlice";
+import { productsApi } from "./apiSlice";
 
 export const store = configureStore({
   reducer: {
     count: counterSlice,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 export default store;
